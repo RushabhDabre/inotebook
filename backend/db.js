4f96dc6bae5b6b63ceae2615a384f421464e637a
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-04-24 22:26:42 +0530

    connected to mongoDb

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017" // mondocompass URI String
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, () => {
+        console.log("connected to Mongo Successfully");
+    })
+}
+
+module.exports = connectToMongo;
