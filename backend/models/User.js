af8d13904bba509b934b65fe0f999aa666e23d6f
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-04-26 17:52:43 +0530

    date 26

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
+    
     name:{
         type: String,
         required: true
