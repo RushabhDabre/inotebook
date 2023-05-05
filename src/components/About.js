94930f7441974953ae0aee0e424087726ae255eb
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-05-05 20:07:42 +0530

    Frontend Setup

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import React, { useContext, useEffect } from 'react';
+// import noteContext from '../context/notes/noteContext';
+
+const About = () => {
+     
+  return (
+    <div>
+        This is About page
+    </div>
+  )
+}
+
+export default About
