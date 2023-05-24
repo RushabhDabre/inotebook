5a1533606a1b2858be8cc2db479143252697e787
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-05-25 00:34:47 +0530

    added Alert and Confirmation button

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -3,7 +3,7 @@ import noteContext from "../context/notes/noteContext"
 import NoteItem from './NoteItem';
 import AddNote from './AddNote';
 
-const Notes = () => {
+const Notes = (props) => {
     const context = useContext(noteContext);
     const { notes, getNotes, editNote } = context;
     useEffect(() => {
@@ -24,6 +24,7 @@ const Notes = () => {
     const handleClick = (e) => {
         editNote(note.id, note.etitle, note.edescription, note.etag);
         refClose.current.click();
+        props.showAlert("Updated successfully", "success");
     }
 
     const onChange = (e) => {
@@ -32,7 +33,7 @@ const Notes = () => {
 
     return (
         <>
-            <AddNote />
+            <AddNote showAlert={props.showAlert}/>
 
             <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Launch demo modal
@@ -77,7 +78,7 @@ const Notes = () => {
                     {notes.length === 0 && 'No notes to display'}
                 </div>
                 {notes.map((note) => {
-                    return <NoteItem key={note._id} updateNote={updateNote} note={note} />;
+                    return <NoteItem key={note._id} updateNote={updateNote} showAlert={props.showAlert} note={note} />;
                 })}
             </div>
         </>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-function Signup() {
+function Signup(props) {
   const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" });
   let navigate = useNavigate();
   const handleSubmit = async (e) => {
@@ -20,9 +20,10 @@ function Signup() {
       // Save the auth token and redirect
       localStorage.setItem('token', json.authtoken);
       navigate("/");
+      props.showAlert("Account Created Successfully", "success")
     }
     else {
-      alert("Invalid credentials");
+      props.showAlert("Invalid Details", "danger")
     }
 
   }
