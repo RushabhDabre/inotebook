c4215dd4ee3a958bc7dc489d629454f0f652d7b3
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-05-24 19:36:28 +0530

    login setup

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -35,7 +35,7 @@ const AddNote = () => {
                     <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange} minLength={5} required />
                 </div>
                 
-                <button disabled={note.title.length < 5 || note.description.length < 5} type="submit" className="btn btn-primary" onClick={handleClick} >Add Note</button>
+                <button disabled={note.title.length < 5 || note.description.length < 5} type="submit" className="btn btn-dark" onClick={handleClick} >Add Note</button>
             </form>
         </div>
 
