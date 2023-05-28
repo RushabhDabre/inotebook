806128100d5f10f173102bd653d066aad4546681
Rushabh Dabre <rushabhdabare14@gmail.com>
2023-05-28 20:49:09 +0530

    fixed

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -37,7 +37,7 @@ const NoteItem = (props) => {
                     <p className="card-text">{note.description}</p>
 
                     <i className="fa-solid fa-trash mx-2" onClick={() => { handleDelete(); }}></i>
-                    <i className="fa-solid fa-pen-to-square mx-2" onClick={() => { updateNote(note); showAlert("Updated successfully", "success"); }}></i>
+                    <i className="fa-solid fa-pen-to-square mx-2" onClick={() => { updateNote(note);  }}></i>
                     {/* <i className="fa-solid fa-trash mx-2" onClick={handleDelete}></i> */}
                     {/* <i className="fa-solid fa-pen-to-square mx-2" onClick={() => updateNote(note)}></i> */}
                 </div>
